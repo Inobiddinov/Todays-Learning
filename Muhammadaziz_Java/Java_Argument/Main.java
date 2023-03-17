2211f374a0ae82c34838f3fb32f2f1e97b62ba37
mukhammad aziz <inobiddinovmuhammadaziz@gmail.com>
2023-03-17 20:54:46 +0800

    okay

---
+++ Muhammadaziz_Java/Java_Argument/Main.java
@@ -0,0 +1,9 @@
+package Java_Argument;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println(args[2]);
+    }
+}
